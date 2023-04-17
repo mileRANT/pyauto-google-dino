620c31245f0729626e70fe75649c67cc61b7f057
mileRANT <liem.htran@outlook.com>
2023-04-17 15:45:43 -0400

    Setting up the rectangle in front of the dino. used in full screen

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pyautogui 
+from PIL import Image, ImageGrab 
+import time
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# basic function for clicking a key
+def click(key):
+   pyautogui.keyDown(key)
+   return
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    time.sleep(5)
+    click('up')
+
+    while True:  # for infinity loop
+       # capture image in black & white format
+       image = ImageGrab.grab().convert('L')
+       data = image.load()
+
+       # Draw the rectangle for cactus
+       for i in range(740, 820):
+          for j in range(395, 425):
+             data[i, j] = 0
+
+       # Draw the rectangle for birds
+       for i in range(740, 770):
+          for j in range(365, 390):
+             data[i, j] = 171
+       image.show()
+       break
+
+#
+# def isCollision(data):
+# # Check colison for birds
+#     for i in range(530,560):
+#         for j in range(80, 127):
+#             if data[i, j] < 171:
+#                 click("down")
+#                 return
+#  # Check colison for cactus
+#     for i in range(530, 620):
+#         for j in range(130, 160):
+#             if data[i, j] < 100:
+#                 click("up")
+#                 return
+#     return
