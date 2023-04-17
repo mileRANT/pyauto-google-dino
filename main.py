ba34e295663916a7c45c04fbb9ba4835ab7b5802
mileRANT <liem.htran@outlook.com>
2023-04-17 16:05:50 -0400

    updated hitboxes, opens chrome browser as well.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyautogui 
 from PIL import Image, ImageGrab 
 import time
+import webbrowser
 
 
 # basic function for clicking a key
@@ -11,13 +12,13 @@ def click(key):
 
 def isCollision(data):
 # # Check colison for birds
-   for i in range(770, 800):
+   for i in range(760, 790):
       for j in range(365, 390):
             if data[i, j] < 171:
                 click("down")
                 return
    # Check colison for cactus
-   for i in range(770, 850):
+   for i in range(760, 840):
       for j in range(395, 425):
             if data[i, j] < 100:
                 click("up")
@@ -35,7 +36,7 @@ def isCollision(data):
 #       for i in range(770, 850):
 #          for j in range(395, 425):
 #             data[i, j] = 0
-# 
+#
 #       # Draw the rectangle for birds
 #       for i in range(770, 800):
 #          for j in range(365, 390):
@@ -44,13 +45,20 @@ def isCollision(data):
 #       break
 
 if __name__ == "__main__":
-    time.sleep(5)
-    click('up')
+   chrome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
-    while True:
-        image = ImageGrab.grab().convert('L')
-        data = image.load()
-        isCollision(data)
+   # open a public URL, in this case, the webbrowser docs
+   url = "https://elgoog.im/t-rex/"
+   webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+   webbrowser.get('chrome').open_new_tab(url)
+
+   time.sleep(5)
+   click('up')
+
+   while True:
+     image = ImageGrab.grab().convert('L')
+     data = image.load()
+     isCollision(data)
 
 
 
