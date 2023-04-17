4051bae553b939868368b39be216cda00539a248
mileRANT <liem.htran@outlook.com>
2023-04-17 15:58:16 -0400

    Adjusting hit collision detection for accuracy.

--- main.py
+++ main.py
@@ -9,39 +9,48 @@ def click(key):
    return
 
 
+def isCollision(data):
+# # Check colison for birds
+   for i in range(770, 800):
+      for j in range(365, 390):
+            if data[i, j] < 171:
+                click("down")
+                return
+   # Check colison for cactus
+   for i in range(770, 850):
+      for j in range(395, 425):
+            if data[i, j] < 100:
+                click("up")
+                return
+   return
+
+# this code was to show us the hit boxes. upon confirming the hitboxes are in the correct area,
+# we set it up in the iscollision method
+#    while True:  # for infinity loop
+#       # capture image in black & white format
+#       image = ImageGrab.grab().convert('L')
+#       data = image.load()
+#
+#       # Draw the rectangle for cactus
+#       for i in range(770, 850):
+#          for j in range(395, 425):
+#             data[i, j] = 0
+# 
+#       # Draw the rectangle for birds
+#       for i in range(770, 800):
+#          for j in range(365, 390):
+#             data[i, j] = 171
+#       image.show()
+#       break
+
 if __name__ == "__main__":
     time.sleep(5)
     click('up')
 
-    while True:  # for infinity loop
-       # capture image in black & white format
-       image = ImageGrab.grab().convert('L')
-       data = image.load()
+    while True:
+        image = ImageGrab.grab().convert('L')
+        data = image.load()
+        isCollision(data)
 
-       # Draw the rectangle for cactus
-       for i in range(740, 820):
-          for j in range(395, 425):
-             data[i, j] = 0
 
-       # Draw the rectangle for birds
-       for i in range(740, 770):
-          for j in range(365, 390):
-             data[i, j] = 171
-       image.show()
-       break
 
-#
-# def isCollision(data):
-# # Check colison for birds
-#     for i in range(530,560):
-#         for j in range(80, 127):
-#             if data[i, j] < 171:
-#                 click("down")
-#                 return
-#  # Check colison for cactus
-#     for i in range(530, 620):
-#         for j in range(130, 160):
-#             if data[i, j] < 100:
-#                 click("up")
-#                 return
-#     return
